a2547f1cb7dbfe8124e930578597a7b3c688115c
Chandrasekhar C.A <chandracasekhar@gmail.com>
2023-11-25 03:51:45 +0530

    Handling Pdf text with PalmAPI

--- back-end/palmInt.js
+++ back-end/palmInt.js
@@ -1,5 +1,5 @@
-// palmInt.js
-import dotenv from 'dotenv';
+import { processPDFs } from "./pdfProcesssor.js";
+import dotenv from "dotenv";
 dotenv.config();
 import { TextServiceClient } from "@google-ai/generativelanguage";
 import { GoogleAuth } from "google-auth-library";
@@ -12,7 +12,6 @@ const client = new TextServiceClient({
 });
 
 async function generateRepeatedPhrases(text) {
-  
   const prompt = `Find the most repeated phrases in this text. give me the top 5 most repeated phrases. the phrases should be minimum 5 words long. text: ${text}`;
   try {
     const result = await client.generateText({
@@ -21,21 +20,28 @@ async function generateRepeatedPhrases(text) {
         text: prompt,
       },
     });
-    if (result && result[0] && result[0].candidates && result[0].candidates.length > 0) {
+    if (
+      result &&
+      result[0] &&
+      result[0].candidates &&
+      result[0].candidates.length > 0
+    ) {
       const outputText = result[0].candidates[0].output;
       console.log(outputText);
       return outputText;
     } else {
-      console.log('No output text found.');
-      return '';
+      console.log("No output text found.");
+      return "";
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     throw error;
   }
 }
 
-const sampletext = 'random sentences used for testing this amazing project';
-generateRepeatedPhrases(sampletext);
+const usePDFText = async (combinedText) => {
+  await generateRepeatedPhrases(combinedText);
+};
 
+export { usePDFText };
 export default generateRepeatedPhrases;
--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import multer from "multer";
-import fs from "fs";
 import path from "path";
 import cors from "cors";
-import PDFParser from "pdf2json";
+import { processPDFs } from "./pdfProcesssor.js";
+import { usePDFText } from "./palmInt.js";
 
 const app = express();
 app.use(cors());
@@ -25,43 +25,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 app.post("/uploadpdf", upload.array("files", 10), async (req, res) => {
-  const allTexts = [];
-
-  const processPDF = (file) => {
-    return new Promise((resolve, reject) => {
-      const pdfParser = new PDFParser();
-
-      pdfParser.on("pdfParser_dataError", (errData) =>
-        reject(errData.parserError)
-      );
-      pdfParser.on("pdfParser_dataReady", (pdfData) => {
-        const text = pdfData.formImage.Pages.map((page) => {
-          return page.Texts.map((text) => decodeURIComponent(text.R[0].T)).join(
-            " "
-          );
-        }).join("\n");
-
-        allTexts.push(text);
-        resolve();
-      });
-
-      pdfParser.loadPDF(file.path);
-    });
-  };
-
   try {
-    await Promise.all(req.files.map((file) => processPDF(file)));
-
-    // Combine all texts and save to a single file
-    const combinedText = allTexts.join("\n\n");
-    const outputPath = path.join(
-      "uploads",
-      "text",
-      `combined-${Date.now()}.txt`
-    );
-    fs.writeFileSync(outputPath, combinedText);
-
-    res.send("Files uploaded and combined into a single text file.");
+    const combinedText = await processPDFs(req.files);
+    await usePDFText(combinedText); // Pass the combined text to usePDFText
+    res.send("Files processed successfully.");
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Error in processing the PDF files");
@@ -75,5 +42,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// Ensure that the 'uploads' and 'uploads/text' directories exist in your project folder.
