1c93842590b2de02d4fe6af493f49ff5d118ad4c
Adil <muhamedadil03@gmail.com>
2023-11-24 13:35:24 +0530

    frontend

---
+++ front-end/my-angular-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FileUploadComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
