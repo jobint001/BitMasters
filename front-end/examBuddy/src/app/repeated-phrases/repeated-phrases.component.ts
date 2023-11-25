4012ebddb5a2723eb0261bb246ca624e5782a381
Chandrasekhar C.A <chandracasekhar@gmail.com>
2023-11-25 10:27:36 +0530

    Final Changes

--- front-end/examBuddy/src/app/repeated-phrases/repeated-phrases.component.ts
+++ front-end/examBuddy/src/app/repeated-phrases/repeated-phrases.component.ts
@@ -4,16 +4,16 @@ import { DataService } from '../shared/data.service';
 @Component({
   selector: 'app-repeated-phrases',
   templateUrl: './repeated-phrases.component.html',
-  styleUrls: ['./repeated-phrases.component.css']
+  styleUrls: ['./repeated-phrases.component.css'],
 })
 export class RepeatedPhrasesComponent implements OnInit {
-  phrases: string[] = [];
+  phrases: string[][] = []; // Array of arrays to hold split lines
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
-    this.dataService.phrases$.subscribe((phrases) => {
-      this.phrases = phrases;
+    this.dataService.phrases$.subscribe((phrasesArray: string[]) => {
+      this.phrases = phrasesArray.map((phrase) => phrase.split('\\n'));
     });
   }
 }
